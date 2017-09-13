9da2add964fa063f2728a4fac59c16bc5ac32b58
Jianyu Li <joannyli@foxmail.com>
2017-09-13 11:38:29 +0800

    Just forbid standby-replay mds export dirs
    Signed-off-by: Jianyu Li <joannyli@tencent.com>

--- src/mds/Migrator.cc
+++ src/mds/Migrator.cc
@@ -793,8 +793,8 @@ void Migrator::export_dir(CDir *dir, mds_rank_t dest)
     //ceph_abort();
     return;
   }
-  if (!mds->is_active()) {
-    dout(7) << "i'm not active, no exports for now" << dendl;
+  if (!mds->is_standby_replay()) {
+    dout(7) << "i'm standby replay, no exports for now" << dendl;
     return;
   }
 
